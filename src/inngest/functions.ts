6d61f4f7221eea34bc5f965c138644d02d93e9a3
RuslanIman-Aliev <s0970802047@gmail.com>
2026-01-30 23:39:10 +0100

    07: ai integration

---
+++ src/inngest/functions.ts
@@ -0,0 +1,29 @@
+import { createGoogleGenerativeAI } from "@ai-sdk/google";
+import { createOpenAI } from "@ai-sdk/openai";
+import { generateText } from "ai";
+import { inngest } from "./client";
+
+const google = createGoogleGenerativeAI();
+const openai = createOpenAI();
+
+export const execute = inngest.createFunction(
+  { id: "execute-ai" },
+  { event: "execute/ai" },
+  async ({ event, step }) => {
+    const { steps: gemini } = await step.ai.wrap("gemini-generate-text", generateText, {
+      model: google("gemini-2.5-flash"),
+      system: "You are a helpful assistant that helps users with their tasks.",
+      prompt: "2+2",
+    });
+
+     const { steps: openAI } = await step.ai.wrap("openai-generate-text", generateText, {
+      model: openai("gpt-4o"),
+      system: "You are a helpful assistant that helps users with their tasks.",
+      prompt: "2+2",
+    });
+
+    return {
+      gemini,openAI
+    }
+  },
+);
