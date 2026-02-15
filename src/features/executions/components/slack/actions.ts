226fc4212439605d2f418a9f6cb8919b1c2e1f45
RuslanIman-Aliev <s0970802047@gmail.com>
2026-02-15 21:30:35 +0100

    26-discord-slack-node

--- src/features/executions/components/slack/actions.ts
+++ src/features/executions/components/slack/actions.ts
@@ -2,13 +2,9 @@
 
 import { getSubscriptionToken, type Realtime } from "@inngest/realtime";
 import { inngest } from "@/inngest/client";
-import { discordRequestChannel } from "@/inngest/channels/discord";
 import { slackRequestChannel } from "@/inngest/channels/slack";
 
-export type SlackToken = Realtime.Token<
-  typeof  slackRequestChannel,
-  ["status"]
->;
+export type SlackToken = Realtime.Token<typeof slackRequestChannel, ["status"]>;
 
 export async function fetchSlackRealtimeToken(): Promise<SlackToken> {
   const token = await getSubscriptionToken(inngest, {
